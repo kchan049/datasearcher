576d9940dc83da5b16a276a517124dd5ffe32720
Kenneth Chan <KennethChan@Kenneths-MBP.attlocal.net>
2015-11-24 13:39:46 -0800

    added small file and lists for data set

--- dataclassifier.py
+++ dataclassifier.py
@@ -3,8 +3,22 @@
 import time
 import re
 import sys, os, traceback, optparse
+import struct
 
 def main():
-	pass
+	largedataset = []
+	smalldataset = []
+	get_decimals_from_file("cs_170_small4.txt", smalldataset)
+
+def get_decimals_from_file(file_name, dataset):
+	file = open(file_name)
+	for i in file:
+		strdecimal = file.readline().split()
+		dataset.append(list(map(float,strdecimal)))
+	for i in dataset:
+		print(i)
+	
+	#strdecimal = file.readline().split()
+	#return map(float, strdecimal)
 if __name__ == '__main__':
 	sys.exit(main())
